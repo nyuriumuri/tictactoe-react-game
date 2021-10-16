453b33428fc3cf23c337133dc8dc8e95ff0ca607
nyuriumuri <nyuriumuri@gmail.com>
2021-10-16 13:11:10 +0200

    Updates

---
+++ precache-manifest.d2018584d93a8ee2be8c871a6bf6e6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4ffd2f2bd272598dac8eeda8710966",
+    "url": "/tictactoe-react-game/index.html"
+  },
+  {
+    "revision": "83175da0fbc3a4c2ba63",
+    "url": "/tictactoe-react-game/static/css/main.8a2be3aa.chunk.css"
+  },
+  {
+    "revision": "21466152d63bbd68a232",
+    "url": "/tictactoe-react-game/static/js/2.a1c3202a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tictactoe-react-game/static/js/2.a1c3202a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83175da0fbc3a4c2ba63",
+    "url": "/tictactoe-react-game/static/js/main.6f889063.chunk.js"
+  },
+  {
+    "revision": "7028395d2727b3512efd",
+    "url": "/tictactoe-react-game/static/js/runtime-main.499cdd21.js"
+  }
+]);
